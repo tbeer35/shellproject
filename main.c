bb2c96c3d9211e5612862cfe7444306f933e6d74
tracy <tracybeer35@gmail.com>
2023-03-17 19:21:33 -0400

    worked on pipes and getting them to work

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include <errno.h>
 #include <signal.h>
 #include <dirent.h>
+#include <stdbool.h>
 
 //declare all functions we use later
 void end();
 void handleSigInt(int sig);
-int handleLS(int wordNum, char *words[]);
+int handleLS(int wordNum, char *words[], int curWord, bool pipe);
 int handleCD(int wordNum, char *words[]);
 int handleDog();
 void handlePWD();
@@ -21,14 +22,15 @@ int main(int argc, char **argv) {
 }
 
 //takes in the input in a char** and how much there is and calls various other functions to handle each one
-void executeInput(char *words[], int wordNum){
+void executeInput(char *words[], int wordNum, bool pipe){
 	int x =0;
         for (int j = 0; j < wordNum; j++) //loop over all input
                 printf("words[%d]: %s\n", j, words[j]);
                 if(strcmp(words[x], "exit")==0){
                         end();
                 }else if(strcmp(words[x], "ls")==0){
-			handleLS(wordNum, words);
+			x = handleLS(wordNum, words, x, pipe);
+			pipe = false;
                 }else if(strcmp(words[x], "cd")==0){
 			handleCD(wordNum, words);
 		}else if(strcmp(words[x], "dog")==0){
@@ -53,6 +55,7 @@ void parseInput(int argc, char** argv) {
     char *words_on_line[10]; // 10 words on a line
     int stop = 0;
     while(1){ //loop until the shell is killed
+	    bool pipe = false;
     	signal(SIGINT, handleSigInt); //catch ctrl-C
     	if (fgets(buf, 100, stdin) == NULL && feof(stdin)){ //catch ctrl-D
         	end(); //call end
@@ -71,7 +74,12 @@ void parseInput(int argc, char** argv) {
         	*s = 0;
     	}
 	//then call executeInput giving it the input and how much there is
-    	executeInput(words_on_line, i);
+	for(int j = 0; j <= i; j++){
+		if(strcmp(words_on_line[j], "|") == 0){
+			pipe = true;
+		}
+	}
+    	executeInput(words_on_line, i, pipe);
     	//for (int j = 0; j < i; j++)
           //  	printf("words_on_line[%d]: %s\n", j, words_on_line[j]);
     	//if (strcmp(words_on_line[0],"stop") == 0)
@@ -85,18 +93,21 @@ void end(){
         exit(-1);
 }
 
-int handleLS(int wordNum, char *words[]) {
+int handleLS(int wordNum, char *words[], int curWord, bool pipe) {
+    printf("curWord is %d\n", curWord);
     DIR *dir;
     struct dirent *ent;
-	int y = 0;
-	while (y < wordNum) {     
-	    for (int z = 0; z < 10; z++) {
-		if (words[y] != "&" || words[y] < 10){
+	int startWord = curWord;	
+	    //for (int z = 0; z < 10; z++) {
+	while(curWord <= wordNum){
+		printf("in the while\n");
+	   if (words[curWord] != "&" && pipe == false|| words[curWord] <= wordNum && pipe == false){
+		   printf("in the if\n");
 
 		// Open the current directory or a chosen directory as an argument
-		if (wordNum == 1) {
+		if (wordNum == 1 || strcmp(words[curWord+1], "")) {
 		    dir = opendir(".");
-		} else if (wordNum == 2) {
+		} else if (wordNum == 2 || strcmp(words[curWord+2], "")) {
 		    dir = opendir(words[1]);
 		} else {
 		    printf("Usage: ls [dir]\n");
@@ -105,7 +116,7 @@ int handleLS(int wordNum, char *words[]) {
 		// if no directory it throws the error message
 		if (dir == NULL) {
 		    printf("Error: Cannot open directory\n");
-		    exit(1);
+		    break;
 		}
 
 		// Loop through all the entries in the directory
@@ -115,18 +126,51 @@ int handleLS(int wordNum, char *words[]) {
 
 		// Close the directory
 		closedir(dir);
-
 		return 0;
-	    } else if (words[y] == "&") {
+	    } else if (words[curWord] == "&" && pipe == false) {
 		//Run process as background
+		printf("in the else if\n");
 		int bgProcess = fork();
 		printf("%s running in background", &bgProcess);	
-	    } else if (words[y] == "|") {
+	    } else if (pipe == true) {
 		//Run pipe command
+		if(fork() == 0){
+			printf("in child, curWord is %d\n", curWord);
+			//child calls LS
+			if (strcmp(words[curWord+1],"") == 0 || strcmp(words[curWord+1],"|")==0) {
+		                printf("in the first if\n");
+				dir = opendir(".");
+                        } else if (strcmp(words[curWord+2],"") == 0 || strcmp(words[curWord+2],"|")==0) {
+				printf("in the second if\n");
+				dir = opendir(words[startWord+1]);
+	                } else {
+ 	                        printf("Usage: ls [dir]\n");
+	                        exit(1);
+	                }
+                        // if no directory it throws the error message
+	                if (dir == NULL) {
+	                        printf("Error: Cannot open directory\n");
+				kill(getpid(), SIGINT);
+                        }
+                        // Loop through all the entries in the directory
+                        while ((ent = readdir(dir)) != NULL) {
+	                        printf("%s\n", ent->d_name);
+                        }
+                        // Close the directory
+                        closedir(dir);
+			pipe = false;
+			kill(getpid(), SIGINT);
+			
+		}else{
+			//parent calls whatever it needs to
+			printf("in parent");
+			return curWord;
+		}
+
 
 	    }
-	y++;
-	}
+	printf("about to iterate curWord\n");
+	curWord++;
 	}
 }
 
