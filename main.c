80aad968cbb01b9ab9f43754c2cb6352cc071c1a
MaxDupont02 <edupont@mail.umw.edu>
2023-03-17 00:37:38 -0400

    commented out not working dog parts

--- main.c
+++ main.c
@@ -144,29 +144,29 @@ int handleDog() {
         printf("Error opening file.");
         return 1;
     }
-
-    printf("What would you like to write in the file: ");
-    char buf[100]; // buffer for line
-    char whitespace[] = " \t\r\n\v";
-    char *words_on_line[10]; // 10 words on a line
-    int stop = 0;
-    fgets(buf, 100, stdin);
-    char *s = buf;
-    char *end_buf = buf + strlen(buf);
-    int eol = 0, i = 0;
-    while (1) {
-	while (s < end_buf && strchr(whitespace, *s))
-    		s++;
-    	if (*s == 0) // eol - done
-	        break;
-	words_on_line[i++] = s;
-	while (s < end_buf && !strchr(whitespace, *s))
-		s++;
-	*s = 0;
-    }
-    for(int x = 0; x < i; i++){
-    	fprintf(file_ptr, "%s ",words_on_line[x]); // write to file
-    }
+//    printf("What would you like to write in the file: ");
+//    char buf[100]; // buffer for line
+//    char whitespace[] = " \t\r\n\v";
+//    char *words_on_line[10]; // 10 words on a line
+//    int stop = 0;
+//    fgets(buf, 100, stdin);
+//    char *s = buf;
+//   char *end_buf = buf + strlen(buf);
+//    int eol = 0, i = 0;
+//    while (1) {
+//	while (s < end_buf && strchr(whitespace, *s))
+//   		s++;
+//    	if (*s == 0) // eol - done
+//	        break;
+//	words_on_line[i++] = s;
+//	while (s < end_buf && !strchr(whitespace, *s))
+//		s++;
+//	*s = 0;
+//    }
+//    for(int x = 0; x < i; i++){
+//    	fprintf(file_ptr, "%s ",words_on_line[x]); // write to file
+//    }
+    
     fclose(file_ptr); // close file
 
     return 0;
